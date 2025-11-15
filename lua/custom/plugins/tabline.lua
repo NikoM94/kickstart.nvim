9a4840f1e2b136d0f46dedf9bae539b5a94c1a5c
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-11-15 22:39:52 +0200

    change lualine theme

--- lua/custom/plugins/tabline.lua
+++ lua/custom/plugins/tabline.lua
@@ -1,15 +1,24 @@
 return {
   'kdheepak/tabline.nvim',
+  event = 'BufReadPre',
   config = function()
     require('tabline').setup {
       options = {
         show_filename_only = true,
-      },
-      keys = {
-        { '<S-l>', '<cmd>:TabLineBufferNext<CR>', desc = 'Go to next buffer' },
-        { '<S-h>', '<cmd>:TabLineBufferPrevious<CR>', desc = 'Go to previous buffer' },
-        { '<leader>bo', '<cmd>:BufferLineCloseOthers<CR>', desc = 'Close other buffers' },
+        offsets = {
+          {
+            filetype = 'snacks_layout_box',
+            text = 'Explorer',
+            highglight = 'Directory',
+            text_align = 'left',
+          },
+        },
       },
     }
   end,
+  keys = {
+    { '<S-l>', '<cmd>:TablineBufferNext<CR>', desc = 'Go to next buffer' },
+    { '<S-h>', '<cmd>:TablineBufferPrevious<CR>', desc = 'Go to previous buffer' },
+    { '<leader>bo', '<cmd>:BufferLineCloseOthers<CR>', desc = 'Close other buffers' },
+  },
 }
