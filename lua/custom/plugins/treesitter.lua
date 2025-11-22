c38604f8c7725c06a297e91fccc3daf27215a133
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-11-22 02:32:50 +0200

    smear cursor

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -36,22 +36,6 @@ return {
     },
     config = function(_, opts)
       -- install parsers from custom opts.ensure_installed
-      if opts.ensure_installed and #opts.ensure_installed > 0 then
-        require('nvim-treesitter').install(opts.ensure_installed)
-        -- register and start parsers for filetypes
-        for _, parser in ipairs(opts.ensure_installed) do
-          local filetypes = parser -- In this case, parser is the filetype/language name
-          vim.treesitter.language.register(parser, filetypes)
-
-          vim.api.nvim_create_autocmd({ 'FileType' }, {
-            pattern = filetypes,
-            callback = function(event)
-              vim.treesitter.start(event.buf, parser)
-            end,
-          })
-        end
-      end
-
       -- Auto-install and start parsers for any buffer
       vim.api.nvim_create_autocmd({ 'BufRead' }, {
         callback = function(event)
